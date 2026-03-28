2e287ad5c9a8c7cb7c8397d8b95031312a55b031
Dmitry Radkovskiy <zlumer@gmail.com>
2026-03-28 16:05:20 +0000

    refactor: remove unused package.json import in cli config

--- cli/tsdown.config.ts
+++ cli/tsdown.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from 'tsdown';
-import fs from 'node:fs';
-
-const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
 
 export default defineConfig({
   entry: ['./src/hb.ts'],
